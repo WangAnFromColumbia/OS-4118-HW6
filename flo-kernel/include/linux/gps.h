fc62eed45297c7860f3bba95cdbe53b71cfb049c
Luyao Huang <w4118@w4118>
2015-12-14 16:04:59 -0500

    helper functions declared.

--- flo-kernel/include/linux/gps.h
+++ flo-kernel/include/linux/gps.h
@@ -5,6 +5,11 @@ struct gps_location {
 	double latitude;
 	double longitude;
 	float  accuracy;  /* in meters */
+	float time;
 };
+long long get_longitude();
+long long get_latitude();
+long get_accuracy();
+long location_time();
 
 #endif
