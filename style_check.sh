463ff26311e7c68e3f53425d80f298a5e870f983
chrishsu <chia-hao.hsu@aiesec.net>
2015-12-14 23:16:38 -0500

    Add chen yu style checker

--- style_check.sh
+++ style_check.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 git diff 18cb0432f97f577e66592425b799d6747ef3984d -- '*.c' '*.h' '*.S' > srcdiff
-checkpatch.pl --ignore FILE_PATH_CHANGES -terse --no-signoff -no-tree --summary-file srcdiff
+./checkpatch.pl --ignore FILE_PATH_CHANGES -terse --no-signoff -no-tree --summary-file srcdiff
 rm srcdiff
