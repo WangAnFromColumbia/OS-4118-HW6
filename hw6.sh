5b2d9168f8c726ebdf3e663e01509a40ca9bab64
chrishsu <chia-hao.hsu@aiesec.net>
2015-12-14 21:56:21 -0500

    Modify hw6.sh

--- hw6.sh
+++ hw6.sh
@@ -2,7 +2,7 @@ adb shell rm -rf /data/misc/hmwk6
 
 dd if=/dev/zero of=hmwk6.fs bs=1M count=2
 sudo losetup /dev/loop0 hmwk6.fs
-sudo ./userspace/e2fsprogs/misc/mke2fs -I 256 -t ext3 -L w4118.hmwk6 /dev/loop0
+sudo ./userspace/e2fsprogs/misc/mke2fs -I 256 -t ext4 -L w4118.hmwk6 /dev/loop0
 sudo losetup -d /dev/loop0
 
 adb shell ln -s /dev/block/loop0 /dev/loop0
@@ -17,7 +17,4 @@ adb shell ln -s /dev/block/loop7 /dev/loop7
 adb push hmwk6.fs /data/misc
 adb shell mkdir /data/misc/hmwk6
 
-adb shell mount -o loop -t ext3 /data/misc/hmwk6.fs /data/misc/hmwk6
-
-adb shell mkdir /data/misc/hmwk6/test3
-adb shell cat proc/kmsg
+adb shell mount -o loop,gps_aware_inode -t ext4 /data/misc/hmwk6.fs /data/misc/hmwk6
